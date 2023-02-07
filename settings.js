e21d447fa02f585070d65af4e96c6d39729374a6
crystallg-dot <121391868+crystallg-dot@users.noreply.github.com>
2023-02-07 18:22:53 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc' 
-global.apikey = ["Zeeoneofc", "Alphabot", "Alpha"]
+global.creator = 'ItsHasbi' 
+global.apikey = ["insidebot","Zeeoneofc", "Alphabot", "Alpha"]
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
